e7502b59ed2ede843313b5413d29ab0e38ebeaae
Alex Dachin <adachin@spotify.com>
2026-05-18 21:06:35 +0200

    Upgrade to nvim 0.12 native lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,15 +5,10 @@ return {
     opts = {},
   },
   {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      { "mason-org/mason.nvim", config = true, version = "^1.0.0" },
-      { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-      { "hrsh7th/cmp-nvim-lsp" },
-    },
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { { "mason-org/mason.nvim", opts = {} }, "neovim/nvim-lspconfig" },
     config = function()
       require("mason-lspconfig").setup({
-        automatic_installation = true,
         ensure_installed = {
           "bashls",
           "cssls",
@@ -24,34 +19,42 @@ return {
           "jsonls",
           "lua_ls",
           "rust_analyzer",
+          "starpls",
           "ts_ls",
           "yamlls",
         },
+        automatic_enable = true,
       })
 
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      vim.lsp.config('jdtls', {
+        root_markers = { '.project' },
+      })
 
-      require("mason-lspconfig").setup_handlers({
-        function(server)
-          require("lspconfig")[server].setup({
-            capabilities = capabilities,
-            on_attach = function(_, bufnr)
-              local map = function(mode, keys, func, desc)
-                vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
-              end
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(ev)
+          local map = function(mode, keys, func, desc)
+            vim.keymap.set(mode, keys, func, { buffer = ev.buf, desc = 'LSP: ' .. desc })
+          end
 
-              map({ "n" }, "K", vim.lsp.buf.hover, "Hover")
-              map({ "n" }, "gd", require("telescope.builtin").lsp_definitions, "Definitions")
-              map({ "n" }, "gr", require("telescope.builtin").lsp_references, "References")
-              map({ "n" }, "gl", vim.diagnostic.open_float, "Line Diagnostics")
-              map({ "n" }, "<leader>td", require("telescope.builtin").lsp_type_definitions, "Type Definitions")
-              map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code Action")
-              map({ "n" }, "<leader>rn", vim.lsp.buf.rename, "Rename")
-              map({ "n" }, "<leader>lf", function() vim.lsp.buf.format { async = true } end, "Format Document")
-              map({ "n" }, "<leader>xx", require("telescope.builtin").diagnostics, "All Diagnostics")
-            end,
-          })
+          map('n', 'K', vim.lsp.buf.hover, 'Hover')
+          map('n', 'gd', require('telescope.builtin').lsp_definitions, 'Definitions')
+          map('n', 'grr', require('telescope.builtin').lsp_references, 'References')
+          map('n', 'gl', vim.diagnostic.open_float, 'Line Diagnostics')
+          map('n', '<leader>td', require('telescope.builtin').lsp_type_definitions, 'Type Definitions')
+          map({ 'n', 'v' }, 'gra', vim.lsp.buf.code_action, 'Code Action')
+          map('n', 'grn', vim.lsp.buf.rename, 'Rename')
+          map('n', '<leader>lf', function() vim.lsp.buf.format { async = true } end, 'Format Document')
+          map('n', '<leader>xx', require('telescope.builtin').diagnostics, 'All Diagnostics')
         end,
+      })
+    end,
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    lazy = false,
+    config = function()
+      vim.lsp.config('*', {
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
       })
     end,
   },
--- .config/nvim/lua/plugins/qol.lua
+++ .config/nvim/lua/plugins/qol.lua
@@ -1,7 +1,6 @@
 return {
   { "github/copilot.vim" },
   { "mattn/emmet-vim" },
-  { "numToStr/Comment.nvim" },
   { "tpope/vim-abolish" },
   { "tpope/vim-repeat" },
   { "tpope/vim-surround" },
